1559ede34b2a658147238f43955983652b1cd3e4
Karan Sharma <karansyd4@gmail.com>
2020-04-01 19:43:22 +1100

    Adding splash screen Increased minsdkversion to 21

--- app/src/main/java/com/karansyd4/newsappexercise/MainActivity.kt
+++ app/src/main/java/com/karansyd4/newsappexercise/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.karansyd4.newsappexercise
 
+import android.content.Context
+import android.content.Intent
 import androidx.fragment.app.Fragment
 import com.karansyd4.newsappexercise.ui.base.BaseActivity
 import dagger.android.AndroidInjector
@@ -8,6 +10,12 @@ import dagger.android.support.HasSupportFragmentInjector
 import javax.inject.Inject
 
 class MainActivity : BaseActivity(), HasSupportFragmentInjector {
+
+    companion object {
+
+        @JvmStatic
+        fun createIntent(context: Context): Intent = Intent(context, MainActivity::class.java)
+    }
 
     @Inject
     lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Fragment>
